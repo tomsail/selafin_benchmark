f330d4835b3b2342cdc0bb376c437545df2fdb54
tomsail <saillour.thomas@gmail.com>
2024-02-26 18:13:13 +0100

    [tests] running through whole time index

--- README.md
+++ README.md
@@ -20,15 +20,32 @@ poetry install
 result for the 3D file in `tests/data/r3d_tidal_flats.slf`: 
 ```
 Warning: Using SerafinFile. It is recommended to compile Hermes api
-Time taken by telemac: 0.04706144332885742 seconds
-Time taken by selafin: 0.006855964660644531 seconds
-Time taken by ppUtils: 0.04119277000427246 seconds
-Time taken by pyTelTools: 0.008388519287109375 seconds
+Time taken by pyTelTools: 0.0059587955474853516 seconds
+Time taken by ppUtils: 0.025746583938598633 seconds
+Warning: Using SerafinFile. It is recommended to compile Hermes api
+Time taken by telemac: 0.05089855194091797 seconds
+Time taken by selafin: 0.04546236991882324 seconds
 ```
 with HERMES
 ```
-Time taken by telemac: 0.0003180503845214844 seconds
-Time taken by selafin: 0.007878303527832031 seconds
-Time taken by ppUtils: 0.041509151458740234 seconds
-Time taken by pyTelTools: 0.008388519287109375 seconds
+Time taken by pyTelTools: 0.00857234001159668 seconds
+Time taken by ppUtils: 0.02410888671875 seconds
+Time taken by telemac: 0.0050008296966552734 seconds
+Time taken by selafin: 0.04771304130554199 seconds
+```
+---
+results for a ERA5 monthly wind file (size: 9 GB):
+```
+Time taken by pyTelTools: 6.695765495300293 seconds
+Time taken by ppUtils: 1.4621760845184326 seconds
+Warning: Using SerafinFile. It is recommended to compile Hermes api
+Time taken by telemac: 85.85983943939209 seconds
+Time taken by selafin: 98.93528032302856 seconds
+```
+with HERMES:
+```
+Time taken by pyTelTools: 6.644935131072998 seconds
+Time taken by ppUtils: 529.0695465195996 seconds
+Time taken by telemac: 9.467883110046387 seconds
+Time taken by selafin: 98.02847123146057 seconds
 ```
--- tests/test_perf.py
+++ tests/test_perf.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 from selafin_benchmark.selafin import Selafin
 from selafin_benchmark.telemac_file import TelemacFile
 from selafin_benchmark.selafin_io_pp import ppSELAFIN
@@ -13,7 +14,9 @@ def timer(func):
         end = time.time()
         print(f"Time taken by {func.__name__}: {end - start} seconds")
         return result
+
     return wrapper
+
 
 PERF = pytest.mark.parametrize(
     "f",
@@ -23,34 +26,48 @@ PERF = pytest.mark.parametrize(
     ],
 )
 
-# selafin
-@PERF 
+
+# selafin@PERF
 @timer
 def selafin(f):
     slf = Selafin(f)
-    slf.get_series([10])
+    data = np.zeros((len(slf.tags["times"]), len(slf.varnames), slf.npoin3))
+    for it, t_ in enumerate(slf.tags["times"]):
+        data[it] = slf.get_values(it)
+
 
 # telemacFile
 @timer
-@PERF 
+@PERF
 def telemac(f):
     tel = TelemacFile(f)
-    for varname in tel.varnames:
-        tel.get_data_value(varname, 10)
+    data = np.zeros((len(tel.times), len(tel.varnames), tel.npoin3))
+    for it, t_ in enumerate(tel.times):
+        for iv, varname in enumerate(tel.varnames):
+            data[it, iv] = tel.get_data_value(varname, it)
+
 
 # ppUtils
 @timer
-@PERF 
+@PERF
 def ppUtils(f):
     ppslf = ppSELAFIN(f)
     ppslf.readHeader()
-    ppslf.readVariables(10)
+    ppslf.readTimes()
+    data = np.zeros((len(ppslf.time), len(ppslf.vars), ppslf.NPOIN))
+    for it, t_ in enumerate(ppslf.time):
+        data[it] = ppslf.readVariables(it)
+
 
 # PyTelTools
 @timer
 @PERF
 def pyTelTools(f):
-    with Serafin.Read(f, 'en') as resin:
+    with Serafin.Read(f, "en") as resin:
         resin.read_header()  # fills resin.header (reads mesh)
-        # resin.get_time()  # fills resin.time but not compulsory here
-        resin.read_vars_in_frame(10)  # read all variables of a specific record
+        resin.get_time()  # fills resin.time but not compulsory here
+        data = np.zeros(
+            (resin.header.nb_frames, len(resin.header.var_IDs), resin.header.nb_nodes)
+        )
+        for it in range(resin.header.nb_frames):
+            data[it] = resin.read_vars_in_frame(it)
